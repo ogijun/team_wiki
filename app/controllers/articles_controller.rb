4f64c1977652d83589761b5dc76328d60600ac07
Junya Ogino <ogijun@gmail.com>
2026-06-02 16:09:31 +0900

    feat: persist fuzzy dates from article form params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(title: article_params[:title], created_by: Current.user)
+    assign_fuzzy_dates(@article)
     if @article.save
       ArticleRevisionCreator.call(article: @article, body: article_params[:body], author: Current.user,
                                   tag_names: split_tags(article_params[:tag_names]),
@@ -34,6 +35,8 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    assign_fuzzy_dates(@article)
+    @article.save!
     ArticleRevisionCreator.call(article: @article, body: article_params[:body], author: Current.user,
                                 tag_names: split_tags(article_params[:tag_names]),
                                 edit_summary: article_params[:edit_summary])
@@ -55,10 +58,27 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :body, :tag_names, :edit_summary)
+    params.require(:article).permit(:title, :body, :tag_names, :edit_summary,
+                                    :start_year, :start_month, :start_day, :start_hour, :start_minute,
+                                    :end_year, :end_month, :end_day, :end_hour, :end_minute)
   end
 
   def split_tags(str)
     str.to_s.split(/[,、]/).map(&:strip).reject(&:empty?)
+  end
+
+  def assign_fuzzy_dates(article)
+    starts = FuzzyDate.from_parts(
+      year: article_params[:start_year], month: article_params[:start_month],
+      day: article_params[:start_day], hour: article_params[:start_hour], minute: article_params[:start_minute]
+    )
+    ends = FuzzyDate.from_parts(
+      year: article_params[:end_year], month: article_params[:end_month],
+      day: article_params[:end_day], hour: article_params[:end_hour], minute: article_params[:end_minute]
+    )
+    article.starts_at = starts&.at
+    article.starts_precision = starts&.precision
+    article.ends_at = ends&.at
+    article.ends_precision = ends&.precision
   end
 end
--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -67,4 +67,42 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     end
     assert_equal "記録削除", Activity.where(action: "article.deleted").order(:id).last.subject_label
   end
+
+  test "create stores fuzzy start with year precision" do
+    login
+    post articles_url, params: { article: {
+      title: "年だけ", body: "x",
+      start_year: "1979", start_month: "", start_day: "", start_hour: "", start_minute: ""
+    } }
+    a = Article.find_by(title: "年だけ")
+    assert_equal "year", a.starts_precision
+    assert_equal Time.zone.local(1979, 1, 1), a.starts_at
+    assert_nil a.ends_at
+  end
+
+  test "create stores fuzzy range with day and month precision" do
+    login
+    post articles_url, params: { article: {
+      title: "期間", body: "x",
+      start_year: "1939", start_month: "9", start_day: "1", start_hour: "", start_minute: "",
+      end_year: "1945", end_month: "8", end_day: "", end_hour: "", end_minute: ""
+    } }
+    a = Article.find_by(title: "期間")
+    assert_equal "day", a.starts_precision
+    assert_equal Time.zone.local(1939, 9, 1), a.starts_at
+    assert_equal "month", a.ends_precision
+    assert_equal Time.zone.local(1945, 8, 1), a.ends_at
+  end
+
+  test "update can set fuzzy date" do
+    login
+    article = Article.create!(title: "あとで日付", created_by: @user)
+    ArticleRevisionCreator.call(article: article, body: "本文", author: @user)
+    patch article_url(article), params: { article: {
+      title: article.title, body: "本文",
+      start_year: "2000", start_month: "1", start_day: "", start_hour: "", start_minute: ""
+    } }
+    assert_equal "month", article.reload.starts_precision
+    assert_equal Time.zone.local(2000, 1, 1), article.starts_at
+  end
 end
