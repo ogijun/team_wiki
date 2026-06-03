ed766902ad520561f064e9286d09bef8a6ab8813
Junya Ogino <ogijun@gmail.com>
2026-06-03 16:34:11 +0900

    feat: add provider/uid/avatar_url to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_02_132724) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_03_073335) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -143,12 +143,16 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_02_132724) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "avatar_url"
     t.datetime "created_at", null: false
     t.string "email_address", null: false
     t.string "name"
     t.string "password_digest", null: false
+    t.string "provider"
+    t.string "uid"
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
