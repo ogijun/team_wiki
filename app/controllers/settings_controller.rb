f9cdf07be29dc2e6ac6b0b39b7caf033fefe4807
Junya Ogino <ogijun@gmail.com>
2026-06-07 09:03:53 +0900

    feat: admin site settings page to edit brand name and logo

---
+++ app/controllers/settings_controller.rb
@@ -0,0 +1,23 @@
+class SettingsController < ApplicationController
+  before_action :require_admin
+
+  def edit
+    @setting = SiteSetting.instance
+  end
+
+  def update
+    @setting = SiteSetting.instance
+    @setting.logo.purge if params[:remove_logo] == "1"
+    if @setting.update(setting_params)
+      redirect_to edit_settings_path, notice: "設定を更新しました。"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def setting_params
+    params.require(:site_setting).permit(:brand_name, :logo)
+  end
+end
---
+++ test/controllers/settings_controller_test.rb
@@ -0,0 +1,32 @@
+require "test_helper"
+
+class SettingsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = User.create!(email_address: "set@example.com", name: "S", provider: "discord", uid: "set-user")
+    sign_in_as(@user)
+  end
+
+  test "edit is admin only" do
+    get edit_settings_url
+    assert_redirected_to root_url
+
+    @user.update!(role: "admin")
+    get edit_settings_url
+    assert_response :success
+  end
+
+  test "admin updates brand name" do
+    @user.update!(role: "admin")
+    patch settings_url, params: { site_setting: { brand_name: "サンプルWiki" } }
+    assert_equal "サンプルWiki", SiteSetting.instance.brand_name
+  end
+
+  test "admin can remove logo (falls back to text)" do
+    @user.update!(role: "admin")
+    s = SiteSetting.instance
+    s.logo.attach(io: StringIO.new("x"), filename: "l.png", content_type: "image/png")
+    s.save!
+    patch settings_url, params: { site_setting: { brand_name: "x" }, remove_logo: "1" }
+    assert_not SiteSetting.instance.logo.attached?
+  end
+end
