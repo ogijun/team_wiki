561922572115c572ac6dc44a14e0ad7aef334921
Junya Ogino <ogijun@gmail.com>
2026-06-08 09:02:29 +0900

    feat: transcription edit/update (nested under material)

---
+++ app/controllers/transcriptions_controller.rb
@@ -0,0 +1,34 @@
+class TranscriptionsController < ApplicationController
+  before_action :set_material, only: %i[edit update]
+
+  def index
+    materials = Material.includes(file_attachment: :blob, transcription: :author).select(&:transcribable?)
+    @groups = materials.group_by { |m| m.transcription&.status || "todo" }
+  end
+
+  def edit
+    @transcription = @material.transcription || @material.build_transcription
+  end
+
+  def update
+    @transcription = @material.transcription || @material.build_transcription
+    @transcription.assign_attributes(transcription_params)
+    @transcription.author = Current.user
+    if @transcription.save
+      redirect_to @material, notice: "書き起こしを保存しました。"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_material
+    @material = Material.find(params[:material_id])
+    redirect_to @material, alert: "この資料は書き起こし対象ではありません。" unless @material.transcribable?
+  end
+
+  def transcription_params
+    params.require(:transcription).permit(:body, :status)
+  end
+end
