ab5f5f9fd4b51cf811ccf614be1867e4297ae667
Junya Ogino <ogijun@gmail.com>
2026-06-16 01:53:36 +0900

    test: lock in Diffy diff HTML-escaping (XSS regression for both revision diffs)
    @diff を html_safe で出力する2箇所（記事/文字起こしのリビジョン差分）について、
    本文に <script> を入れても Diffy がエスケープすることを回帰テストで固定。
    崩れたら XSS なので必ず検出できる。

--- test/controllers/revisions_controller_test.rb
+++ test/controllers/revisions_controller_test.rb
@@ -28,6 +28,19 @@ class RevisionsControllerTest < ActionDispatch::IntegrationTest
     assert_select ".diff"
   end
 
+  # @diff は html_safe で出力するため、本文に HTML/スクリプトが入っても Diffy が
+  # エスケープすることをロックする回帰テスト（崩れたら XSS なので必ず気づける）。
+  test "diff escapes HTML in revision bodies (no XSS through the html_safe diff)" do
+    payload = "<script>alert('XSS-DIFF')</script>"
+    a = ArticleRevisionCreator.call(article: @article, body: "安全な行", author: @user)
+    b = ArticleRevisionCreator.call(article: @article, body: "安全な行\n#{payload}", author: @user)
+    get article_revision_url(@article, b), params: { a: a.id }
+    assert_response :success
+    assert_no_match %r{<script>alert\('XSS-DIFF'\)}, @response.body # 生スクリプトとしては出力されない
+    assert_includes @response.body, "&lt;script&gt;"               # エスケープされてテキスト化
+    assert_includes @response.body, "XSS-DIFF"                     # 追加行は差分に現れている
+  end
+
   test "restore creates a new revision from an old body" do
     assert_difference("@article.revisions.count", 1) do
       post restore_article_revision_url(@article, @r1)
